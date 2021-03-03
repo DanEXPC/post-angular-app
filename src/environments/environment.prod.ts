f6d06ec0078323e1961154ef5bf91c6e38ec72e9
Danylo <danylosmahliuk@gmail.com>
2021-03-03 16:30:12 +0200

    Firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAyPXtXoEXV4K_dz8ma478JYeh3zoOPIEE'
 };
