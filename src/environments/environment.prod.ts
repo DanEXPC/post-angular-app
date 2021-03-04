45c7e3862c77d2233e256ec7bafc10d11783ef12
Danylo <danylosmahliuk@gmail.com>
2021-03-04 16:41:24 +0200

    Creating a post in Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyAyPXtXoEXV4K_dz8ma478JYeh3zoOPIEE'
+  apiKey: 'AIzaSyAyPXtXoEXV4K_dz8ma478JYeh3zoOPIEE',
+  fbDBUrl: 'https://angular-blog-fd1bb-default-rtdb.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyAyPXtXoEXV4K_dz8ma478JYeh3zoOPIEE'
+  apiKey: 'AIzaSyAyPXtXoEXV4K_dz8ma478JYeh3zoOPIEE',
+  fbDBUrl: 'https://angular-blog-fd1bb-default-rtdb.firebaseio.com'
 };
 
 /*
