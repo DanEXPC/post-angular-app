8ae114385d68eccf6cb604173edf6f815879f5ea
Danylo <danylosmahliuk@gmail.com>
2021-03-04 10:27:40 +0200

    Adding logic for storing a token

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -10,10 +10,17 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   get token(): string {
-    return '';
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+
+    return localStorage.getItem('fb-token');
   }
 
   login(user: User): Observable<any> {
+    user.returnSecureToken = true;
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
         tap(this.setToken)
@@ -21,14 +28,21 @@ export class AuthService {
   }
 
   logout(): void {
-
+    this.setToken(null);
   }
 
   isAuthenticated(): boolean {
     return !!this.token;
   }
 
-  private setToken(response: FbAuthResponse): void {
-    console.log(response);
+  private setToken(response: FbAuthResponse | null): void {
+    if (response) {
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else {
+      localStorage.clear();
+    }
+
   }
 }
